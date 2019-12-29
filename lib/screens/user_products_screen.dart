1fca2dada9c0cc32f9d194717a89f9898705597f
piyush_k <piyush.kumar@amplussolar.com>
2019-12-29 18:22:08 +0530

    product CRUD completed

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -32,8 +32,9 @@ class UserProductsScreen extends StatelessWidget {
           itemBuilder: (_, i) => Column(
             children: <Widget>[
               UserProductItem(
+                productsData.items[i].id,
                 productsData.items[i].title,
-                productsData.items[i].imageUrl,
+                productsData.items[i].imageUrl
               ),
               Divider(),
             ],
